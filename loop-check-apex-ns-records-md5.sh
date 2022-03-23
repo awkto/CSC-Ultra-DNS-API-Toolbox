1ae52f0e81ed1974aee656fccccc2acc4da59db8
awkto <awkto@outlook.com>
2022-03-22 21:10:04 -0400

    added more info for comparison

--- loop-check-apex-ns-records-md5.sh
+++ loop-check-apex-ns-records-md5.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 export domainlist=$1
 
+echo "Target MD5 is : 0617414fee3e217474d496dc7ac9104a"
+
 for domain in $(cat $domainlist);do \
 	echo "MD5 is "$(./get-dns-record-root.sh $domain NS \
 				| jq -r .rrSets[0].rdata[] \
